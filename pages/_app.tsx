571644dd9fadb834da3ec112ef8d896526a3855c
srgesio <gesioiajm@gmail.com>
2022-09-29 18:04:37 -0300

    fix font import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
